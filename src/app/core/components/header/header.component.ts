f2b6404383ed36d23c2ff84fb9e8f156e9e6ed80
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-02 17:10:47 +0200

    create the header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html',
   styleUrl:'./header.component.scss',
+  imports:[MatToolbarModule,MatSelectModule],
   standalone:true,
 })
-export class header{}
+export class header{
+  selectedValue: string;
+
+  currencies = [
+    {value: 'EGP', viewValue: 'Egyptian Pound'},
+    {value: 'USD', viewValue: 'American Dollar'},
+    {value: 'EUR', viewValue: 'European Euro'},
+  ];
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { header } from "../../core/components/header/header.component";
 
 @Component({
   selector:'app-home',
   templateUrl:'./home.component.html',
   styleUrl:'./home.component.scss',
+  imports:[header],
   standalone:true,
 })
 export class home{}

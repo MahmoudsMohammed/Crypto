466e45cb6bb35a21e4b7bda0fbb73577d734c332
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-02 15:51:50 +0200

    create components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home',loadComponent:()=>import('../app/pages/home/home.component').then(m=>m.home)},
+  {path:'crypto/:id',loadComponent:()=>import('../app/pages/details/details.component').then(m=>m.cryptoDetails)},
+  {path:'**',redirectTo:'home'}
+];
---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'crypto-details',
+  templateUrl:'./details.component.html',
+  styleUrl:'./details.component.scss',
+  standalone:true,
+})
+export class cryptoDetails{}

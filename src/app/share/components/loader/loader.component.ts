e957a7bcd655d207f2eba0d31365694401638d29
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-02 16:01:49 +0200

    add lottiefiles to project

--- src/app/share/components/loader/loader.component.ts
+++ src/app/share/components/loader/loader.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component , CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 @Component({
   selector:'app-loader',
   templateUrl:'./loader.component.html',
   styleUrl:'./loader.component.scss',
-  standalone:true
+  standalone:true,
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class loaderComponent{}

7cac3983a3417f030bd30c1f0544423280b05b3a
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-06 07:57:04 +0200

    create table and intergrate the data

---
+++ src/app/pages/home/currencies-table/currencies-table.component.ts
@@ -0,0 +1,50 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import {CurrenciesTableItem } from './currencies-table-datasource';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { currency } from '../../../share/models/currency.interface';
+import { apiService } from '../../../share/services/api.services';
+import {MatInputModule} from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+@Component({
+  selector: 'app-currencies-table',
+  templateUrl: './currencies-table.component.html',
+  styleUrl: './currencies-table.component.scss',
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule,MatFormFieldModule,MatInputModule,CommonModule]
+})
+export class CurrenciesTableComponent {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<CurrenciesTableItem>;
+  dataSource: MatTableDataSource<currency>;
+  displayedColumns = ['symbol', 'price','changes',"market"];
+
+  constructor(private apiServ:apiService){
+    this.apiServ.getCurrencies('eur').then(res=>res.json()).then(res=>{
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { header } from "../../core/components/header/header.component";
 import { BannerComponent } from "./banner/banner.component";
-import { TableComponent } from "./table/table.component";
+import { CurrenciesTableComponent } from "./currencies-table/currencies-table.component";
+
 
 @Component({
   selector:'app-home',
   templateUrl:'./home.component.html',
   styleUrl:'./home.component.scss',
-  imports:[header,BannerComponent,TableComponent],
+  imports:[header,BannerComponent,CurrenciesTableComponent],
   standalone:true,
 })
 export class home{}

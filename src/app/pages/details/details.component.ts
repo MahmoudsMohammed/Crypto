13d5513da50ab1f117f176d2311ad937553e8600
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-09 07:51:09 +0200

    display the currency details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { apiService } from '../../share/services/api.services';
+import { CurrencyPipe } from '@angular/common';
+import { communicateService } from '../../share/services/communicate.services';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
   standalone: true,
+  imports: [CurrencyPipe],
 })
 export class DetailsComponent implements OnInit {
   data;
+  selectedCurrency: string;
   constructor(
     private activeRouter: ActivatedRoute,
-    private apiServ: apiService
+    private apiServ: apiService,
+    private communicateSer: communicateService
   ) {}
 
   ngOnInit() {
+    // get currency details data
     this.apiServ
       .getCurrencyById(this.activeRouter.snapshot.params['id'])
       .subscribe((res) => {
         this.data = res;
       });
+    // get the using currency
+    this.communicateSer.selected.subscribe((res) => {
+      this.selectedCurrency = res;
+    });
   }
 }
